2bc1575ab570281e1f07b75e9fa8e14b6b21e2b9
Kiran Roge <kiranroge19@gmail.com>
2024-10-03 08:56:27 -0500

    Module-303.1

---
+++ Module-303/Module-303/src/Welcome.java
@@ -0,0 +1,11 @@
+public class Welcome {
+    public static void main(String args[]){
+        System.out.println("Welcome to Java!");
+        System.out.println("This is per scholas java module 303.");
+
+        //This is a java comment type inline comment
+        /*
+        this is multi line comment
+         */
+    }
+}
---
+++ Module-303/Module-303/src/org/perscholas/Lesson303Dot2.java
@@ -0,0 +1,12 @@
+package org.perscholas;
+
+public class Lesson303Dot2 {
+    public static void main (String args[]){
+        System.out.print("This is line 1\n This is line 2\n");
+        System.out.println("This is line 3");
+        System.out.println("This is line 4");
+
+        //If we want to use tab in a string
+        System.out.println("This is a tab ->\t<-");
+    }
+}
