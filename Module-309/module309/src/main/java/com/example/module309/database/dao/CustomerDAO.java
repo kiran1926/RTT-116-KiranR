78e640e2ec9d9c694f925698da5e9ccbbbcd883f
Kiran Roge <kiranroge19@gmail.com>
2024-12-11 12:57:49 -0600

    created customer DAO and used @ autowired to get the customer printed on console

--- Module-309/module309/src/main/java/com/example/module309/database/dao/CustomerDAO.java
+++ Module-309/module309/src/main/java/com/example/module309/database/dao/CustomerDAO.java
@@ -1,4 +1,16 @@
 package com.example.module309.database.dao;
 
-public class CustomerDAO {
+import com.example.module309.database.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CustomerDAO extends JpaRepository<Customer, Long> {
+
+    @Query("select c from Customer c where c.contactFirstname = :firstName")
+    List<Customer> findByFirstName(String firstName);
+
+    List<Customer>findByContactLastname(String lastName);
+
 }
