56c01156c8464c2197c7ea76657c7fe84069af05
Kiran Roge <kiranroge19@gmail.com>
2024-12-13 11:56:00 -0600

    Working on customer SEARCH. created search jsp file, CustomerController.

---
+++ Module-309/module309/src/main/java/com/example/module309/controller/CustomerController.java
@@ -0,0 +1,38 @@
+package com.example.module309.controller;
+
+import com.example.module309.database.dao.CustomerDAO;
+import com.example.module309.database.entity.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class CustomerController {
+
+    @Autowired
+    private CustomerDAO customerDAO;
+
+    @GetMapping("/customer/search")
+    public ModelAndView search(@RequestParam(required = false) String firstName) {
+        ModelAndView response = new ModelAndView();
+
+        //this is essentially translating /WEB-INF/jsp/customer/search.jsp
+        response.setViewName("customer/search");
+
+        //add the search field to the model so we can use it on the jsp page
+        response.addObject("search", firstName);
+
+        if ( firstName != null) {
+            List<Customer> customers = customerDAO.findByFirstName(firstName);
+            response.addObject("customersKey", customers);
+        }
+
+        System.out.println("===here===");
+        return response;
+
+    }
+}
