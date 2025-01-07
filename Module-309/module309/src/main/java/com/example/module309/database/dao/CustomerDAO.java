28846d95278dec8999a6fa5c03d1cb1d465bcc69
Kiran Roge <kiranroge19@gmail.com>
2025-01-07 16:39:42 -0600

    added join column from different tables query

--- Module-309/module309/src/main/java/com/example/module309/database/dao/CustomerDAO.java
+++ Module-309/module309/src/main/java/com/example/module309/database/dao/CustomerDAO.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Map;
 
 public interface CustomerDAO extends JpaRepository<Customer, Long> {
 
@@ -15,5 +16,15 @@ public interface CustomerDAO extends JpaRepository<Customer, Long> {
 
     //adding this for editing customer
     Customer findById (Integer id);
+
+    List<Customer> findByContactLastnameAndContactFirstnameOrderByIdDesc(String contactLastname, String contactFirstname);
+
+    //
+    @Query(value = "select c.customer_name, c.contact_lastname, c.contact_firstname, " +
+            " e.firstname as employee_firstname, e.lastname as employee_lastname " +
+            " from customers c, employees e " +
+            " where c.sales_rep_employee_id = e.id ", nativeQuery = true)
+
+    List<Map<String, Object>> findCustomerWithEmployeeNames();
 
 }
