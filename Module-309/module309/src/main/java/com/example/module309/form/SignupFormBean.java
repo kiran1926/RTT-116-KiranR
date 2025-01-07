7e662e21c6537a9eb40fa8eea1cc37dfcecf47eb
Kiran Roge <kiranroge19@gmail.com>
2025-01-07 16:37:54 -0600

    Some validation for unique email

--- Module-309/module309/src/main/java/com/example/module309/form/SignupFormBean.java
+++ Module-309/module309/src/main/java/com/example/module309/form/SignupFormBean.java
@@ -1,5 +1,6 @@
 package com.example.module309.form;
 
+import com.example.module309.validation.EmailUnique;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,7 +9,7 @@ import lombok.Setter;
 @Setter
 public class SignupFormBean {
 
-    
+    @EmailUnique(message = "Username already exists.")
     @NotEmpty(message = "Username is required.")
     private String username;
 
